caa9222ec58da25c0f67e478350c384aaa21329e
Jiaqi Liu <jackie_liu@Jiaqis-MacBook-Pro.local>
2019-06-24 23:54:31 -0500

    implement all the methods and add iterativeGet method

--- exercises/lists1/IntList.java
+++ exercises/lists1/IntList.java
@@ -9,17 +9,39 @@ public class IntList {
 
 	/** Return the size of the list using... recursion! */
 	public int size() {
-		return 0;
+		if (rest == null) {
+			return 1;
+		}
+		return 1 + rest.size();
 	}
 
 	/** Return the size of the list using no recursion! */
 	public int iterativeSize() {
-		return 0;
+		IntList p = this;
+		int size = 0;
+		while (p != null) {
+			size ++;
+			p = p.rest;
+		}
+		return size;
 	}
 
 	/** Returns the ith value in this list.*/
 	public int get(int i) {
-		return 0;
+		if (i == 0) {
+			return first;
+		}
+
+		return rest.get(i - 1);
+	}
+
+	public int iterativeGet(int i) {
+		IntList p = this;
+
+		for (int j = 0;j < i;j++) {
+			p = p.rest;
+		}
+		return p.first;
 	}
 
 	public static void main(String[] args) {
@@ -27,6 +49,10 @@ public class IntList {
 		L = new IntList(10, L);
 		L = new IntList(5, L);
 
+		System.out.println(L.size());
 		System.out.println(L.iterativeSize());
+		System.out.println(L.get(1));
+		System.out.println(L.iterativeGet(2));
+
 	}
 } 
