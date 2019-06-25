97c127e90ede4966a50f3f01f01805ec563cdd90
Jiaqi Liu <jackie_liu@Jiaqis-MacBook-Pro.local>
2019-06-25 00:13:39 -0500

    implement all the methods

--- exercises/lists1/Lists1Exercises.java
+++ exercises/lists1/Lists1Exercises.java
@@ -3,15 +3,23 @@ public class Lists1Exercises {
       * each element incremented by x. L is not allowed
       * to change. */
     public static IntList incrList(IntList L, int x) {
-        /* Your code here. */
-        return L;        
+        int sizeL = L.size();
+        IntList L1 = new IntList(L.get(0) + x, null);
+        for (int i = 1;i < sizeL;i++) {
+            L1 = new IntList(L.get(i) + x, L1);
+        }
+        return L1;        
     }
 
     /** Returns an IntList identical to L, but with
       * each element incremented by x. Not allowed to use
       * the 'new' keyword. */
     public static IntList dincrList(IntList L, int x) {
-        /* Your code here. */
+        IntList p = L;
+        while (p != null) {
+            p.first = p.first - x;
+            p = p.rest;
+        }
         return L;
     }
 
@@ -26,8 +34,8 @@ public class Lists1Exercises {
         // Test your answers by uncommenting. Or copy and paste the
         // code for incrList and dincrList into IntList.java and
         // run it in the visualizer.
-        // System.out.println(L.get(1));
-        // System.out.println(incrList(L, 3));
-        // System.out.println(dincrList(L, 3));        
+        System.out.println(L.get(1));
+        System.out.println(incrList(L, 3).get(1));
+        System.out.println(dincrList(L, 3).get(1));        
     }
 }
